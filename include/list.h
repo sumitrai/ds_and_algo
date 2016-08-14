4ccf7cf41c77ad2057d2d987620c5786c05aeac7
Sumit Rai <sumitrai96@gmail.com>
2016-08-14 20:36:37 +0530

    * main.c (main): now tests add_node_pos functionality.
    * lib/list.c (add_node_beg): deleted unused variable tmp.
    
    * list.c (add_node_pos): added new function to insert a node at
    	a given position.

--- include/list.h
+++ include/list.h
@@ -20,4 +20,5 @@ void print_all_slist(struct slist *head);
 struct slist *create_node(int val);
 void add_node_beg(struct slist **head, struct slist *node);
 void add_node_end(struct slist *head, struct slist *node);
+void add_node_pos(struct slist **head, struct slist *node, int insert_at_pos);
 #endif
