043e53ea7fa658da2a04411f8b8cc889a81ea571
Sumit Rai <3071133+sumitrai@users.noreply.github.com>
2016-08-14 16:09:00 +0530

    moving local code to github
    See make help on how to compile
    lib/list.c, include/list.h: singly linked list code
    main.c: a sample test program for singly linked list

---
+++ include/list.h
@@ -0,0 +1,23 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include <stdlib.h>
+
+struct slist {
+    int val;
+    struct slist *next;
+};
+
+#define INIT_NODE(node, value) \
+    node.next = NULL; node.val = value
+#define SIZEOF_SLIST (sizeof(struct slist))			   
+#define for_each_node(pos, head) \
+    for (pos = head; pos != NULL; pos = pos->next)
+#define PRINT_NODE(node) \
+    printf("%p: %d\n", node, node->val)
+
+void print_all_slist(struct slist *head);
+struct slist *create_node(int val);
+void add_node_beg(struct slist **head, struct slist *node);
+void add_node_end(struct slist *head, struct slist *node);
+#endif
