cc7111ddb6aacfd69f15099233cfa3b2748f51b7
Sumit Rai <sumit.rai@calsoftinc.com>
2016-08-18 15:42:58 +0530

    adding Singliy linked list problem 1 solution
    * main.c (main): added print_flags so that print_all_slist
    function only prints values (not pointers) and no newline b/w values.
    
    * include/list.h (PRINT_SLIST_FLAG_SKIP_NL_BW_VAL): print value only instead of
    %p: %d (val).
    (PRINT_SLIST_FLAG_SKIP_NL_BW_VAL): print all the values in one line.
    (print_all_slist): takes extra argument flags to change printing behaviour.
    
    * lib/list.c (print_all_slist): added new argument 'flags' to change
    printing behaviour.
    
    * tests/slist.c: sample program to test slist implementation
    * tests/slist_prob1.c: Singly linked list problem 1 solution.
    
    Note: make run_tests will runn all the samples/problems in tests/

--- tests/slist.c
+++ tests/slist.c
@@ -2,24 +2,20 @@
 
 int main()
 {
-    int print_flags = 0;
     struct slist head;
     struct slist *headp = &head;
-
-    print_flags = PRINT_SLIST_FLAG_PRINT_VAL_ONLY;
-    print_flags |= PRINT_SLIST_FLAG_SKIP_NL_BW_VAL;
     
     INIT_NODE(head, 10);
-    print_all_slist(headp, print_flags);
+    print_all_slist(headp, 0);
     add_node_beg(&headp, create_node(20));
     add_node_beg(&headp, create_node(30));
     add_node_beg(&headp, create_node(40));
-    print_all_slist(headp, print_flags);
+    print_all_slist(headp, 0);
     add_node_end(headp, create_node(9));
     add_node_end(headp, create_node(4));
-    print_all_slist(headp, print_flags);
+    print_all_slist(headp, 0);
     add_node_pos(&headp, create_node(5), 5);
     add_node_pos(&headp, create_node(1), 1);
     add_node_pos(&headp, create_node(200), 200);
-    print_all_slist(headp, print_flags);
+    print_all_slist(headp, 0);
 }
