21da237558cc1beaccdf8f2ebfb2623fc35c5a06
Sumit Rai <sumit.rai@calsoftinc.com>
2016-08-29 02:59:50 +0530

    added more error handling code as per Pratik's comments
    * main.c (main): added more error handling code and updated code as per
    new function prototypes.
    
    * lib/binary_tree.c (bt_new_node): prototype changed and added error handling code.
    now returns 1 on failure, 0 on success.
    (bst_insert, bst_lookup): renamed from bt_*, and See (bt_new_node)

--- lib/binary_tree.c
+++ lib/binary_tree.c
@@ -6,32 +6,39 @@
 
 #include "binary_tree.h"
 
-struct bt_node *bt_new_node(int val)
+// create a new binary tree node: return 1 on failure, 0 on success
+int bt_new_node(struct bt_node **bt_node, int val)
 {
-    struct bt_node *bt_node_ptr = malloc(sizeof(struct bt_node));
-    if (bt_node_ptr) {
-        bt_node_ptr->data = val;
-        bt_node_ptr->left = bt_node_ptr->right = NULL;
-    } else {
+    if (bt_node == NULL) {
+        fprintf(stderr, "%s: first argument is NULL\n", __func__);
+        return 1;
+    }
+    *bt_node = malloc(sizeof(struct bt_node));
+    if (*bt_node == NULL) {
         fprintf(stderr, "%s: memory allocation failed: %s\n", __func__,
                 strerror(errno));
+        return 1;
     }
-    return bt_node_ptr;
+    (*bt_node)->data = val;
+    (*bt_node)->left = (*bt_node)->right = NULL;
+    return 0;
 }
 
-void bt_insert(struct bt_node **node, int val)
+// inser a node into binary search tree: return 1 on failure, 0 on success
+int bst_insert(struct bt_node **node, int val)
 {
     assert(node != NULL);
     if (*node == NULL) {
-        *node = bt_new_node(val);
-        assert(*node != NULL);
-    } else if (val <= (*node)->data)
-        bt_insert(&(*node)->left, val);
-    else
-        bt_insert(&(*node)->right, val);
+        if (bt_new_node(node, val)) return 1;
+    } else if (val <= (*node)->data) {
+        if (bst_insert(&(*node)->left, val)) return 1;
+    } else {
+        if (bst_insert(&(*node)->right, val)) return 1;
+    }
+    return 0;
 }
 
-struct bt_node *bt_lookup(struct bt_node *root, int val)
+struct bt_node *bst_lookup(struct bt_node *root, int val)
 {
     // empty root: nothing to search into
     if (root == NULL)
@@ -43,9 +50,9 @@ struct bt_node *bt_lookup(struct bt_node *root, int val)
         else {
             // root is not a match: check the left, right
             if (val < root->data)
-                return bt_lookup(root->left, val);
+                return bst_lookup(root->left, val);
             else
-                return bt_lookup(root->right, val);
+                return bst_lookup(root->right, val);
         }
     }
 }
--- main.c
+++ main.c
@@ -7,24 +7,32 @@
 int main(int argc, char *argv[], char *envp[])
 {
     // Note: Previous Linked-List feedback applied here - root is now on heap
-    struct bt_node *root = bt_new_node(10);
-    if (root == NULL) {
-            fprintf(stderr, "Failed to allocate memory for root node: %s\n",
-                    strerror(errno));
-            return 1;
+    struct bt_node *root = NULL;
+    
+    if (bt_new_node(&root, 10)) {
+        fprintf(stderr, "Failed to allocate memory for root node: %s\n",
+                strerror(errno));
+        return 1;
     }
     
     // Add some more element here
-    bt_insert(&root, 5);
-    bt_insert(&root, 6);
-    bt_insert(&root, 4);
+    if (bst_insert(&root, 5)) {
+        fprintf(stderr, "failed to insert %d\n", 5);
+    }
+    if (bst_insert(&root, 6)) {
+        fprintf(stderr, "failed to insert %d\n", 6);
+    }
+
+    if (bst_insert(&root, 4)) {
+        fprintf(stderr, "failed to insert %d\n", 4);
+    }
 
     // Verify that all elements are inserted
-    printf("%d: %s\n", 10, bt_lookup(root, 10) == NULL ? "not found" : "found");
-    printf("%d: %s\n", 5, bt_lookup(root, 5) == NULL ? "not found" : "found");
-    printf("%d: %s\n", 6, bt_lookup(root, 6) == NULL ? "not found" : "found");
-    printf("%d: %s\n", 4, bt_lookup(root, 4) == NULL ? "not found" : "found");
-    printf("%d: %s\n", 9, bt_lookup(root, 9) == NULL ? "not found" : "found");
+    printf("%d: %s\n", 10, bst_lookup(root, 10) == NULL ? "not found" : "found");
+    printf("%d: %s\n", 5, bst_lookup(root, 5) == NULL ? "not found" : "found");
+    printf("%d: %s\n", 6, bst_lookup(root, 6) == NULL ? "not found" : "found");
+    printf("%d: %s\n", 4, bst_lookup(root, 4) == NULL ? "not found" : "found");
+    printf("%d: %s\n", 9, bst_lookup(root, 9) == NULL ? "not found" : "found");
 
     return 0;
 }
